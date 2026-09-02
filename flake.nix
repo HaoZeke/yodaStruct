f29444c718502a05c3757980587a636b7f68e873
Rohit Goswami <rgoswami@ieee.org>
2026-09-02 04:56:18 -0500

    build: follow the engine v2.7.0 release

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Source only: meson builds seams-core as a static subproject.
     seams-core = {
-      url = "github:HaoZeke/seams-core/3c2c8cf05f6b4d06ce65444f653576ec969f40e8";
+      url = "github:d-SEAMS/seams-core/964c586feb4fbd96b56257c493157e3055b626e8";
       flake = false;
     };
   };
